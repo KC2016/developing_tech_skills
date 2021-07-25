74329ae068852754cfef0c54f1722ba780ccc82b
Karina Condeixa <18695019+KC2016@users.noreply.github.com>
2021-07-25 17:37:32 +0200

    add python courses
    add python from twitch channel teomewhy and the free tasks from the Dataquest course Python for Data Science.

---
+++ PythonForDataScience_Dataquest/command-line-elements/Introduction to the Command Line-383.sh
@@ -0,0 +1,15 @@
+## 2. Why Learn the Command Line? ##
+
+/home/dq$ date
+
+## 3. The Prompt ##
+
+/home/dq$ alsoNotRandom
+
+## 5. More on Commands ##
+
+/home/dq$ diff -qy augustus tv
+
+## 6. Command History ##
+
+/home/dq$ history
